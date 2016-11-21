7a31e07451b74e53b20b0549a89e6f2180fb2235
roctbb <roctbb@gmail.com>
2016-11-21 16:34:11 +0300

    cats review

--- tornado_test/server.py
+++ tornado_test/server.py
@@ -1,19 +1,51 @@
 import tornado.ioloop
 import tornado.web
+import json
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        cute_cats = ['Дэдди', 'Ростик', 'Козиночка', 'Питер', 'Барсик']
-        self.render('index.html', cats = cute_cats)
+        f = open('reviews.json', 'r')
+        text = f.read()
+        f.close()
 
-class GoodbyeHandler(tornado.web.RequestHandler):
+        cat_list = json.loads(text)
+        self.render('review_list.html', cats=cat_list)
+
+class ReviewHandler(tornado.web.RequestHandler):
     def get(self):
-        name = self.get_argument("name", "")
-        self.write("Goodbye "+name+"!")
+        self.render('index.html')
+    def post(self):
+        email = self.get_argument("email", "")
+        name = self.get_argument("cat_name", "")
+        review = self.get_argument("cat_review", "")
+        galochka = self.get_argument("galochka", "no")
+
+        if email!="" and name!="" and review!="":
+            if galochka == "yes":
+                galochka="да"
+            else:
+                galochka="нет"
+
+            self.render('review.html', email=email, name=name, review=review, galochka=galochka)
+
+            f = open('reviews.json', 'r')
+            text = f.read()
+            f.close()
+
+            cat_list = json.loads(text)
+            cat_list.append({"email": email, "name": name, "review": review, "ssit": galochka})
+            text = json.dumps(cat_list)
+
+            f=open("reviews.json", 'w')
+            f.write(text)
+            f.close()
+        else:
+            self.write("заполните все поля")
+
 
 settings = [
     (r"/", MainHandler),
-    (r"/goodbye", GoodbyeHandler),
+    (r"/save", ReviewHandler),
 ]
 
 app = tornado.web.Application(settings)
