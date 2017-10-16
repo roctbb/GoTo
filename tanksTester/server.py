a589390cb2f04dcfd06eb238a4a60980a0c9a957
roctbb <roctbb@gmail.com>
2017-10-16 13:24:12 +0300

    fix dir

--- tanksTester/server.py
+++ tanksTester/server.py
@@ -8,7 +8,9 @@ import json
 import random
 import string
 
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
+DB_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),'tanks.sqlite')
+MAP_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'map.txt')
+
 from time import gmtime, strftime
 def getKey(N):
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
@@ -19,7 +21,7 @@ class MainHandler(tornado.web.RequestHandler):
     def post(self):
         try:
             file = self.request.files['file'][0]
-            conn = sqlite3.connect('tanks.sqlite')
+            conn = sqlite3.connect(DB_PATH)
             c = conn.cursor()
             c.execute("SELECT * FROM players WHERE key = '%s'"%self.get_argument("key"))
             player = c.fetchone()
@@ -60,7 +62,7 @@ class RegisterHandler(tornado.web.RequestHandler):
         if name is None:
             self.redirect('/register')
         else:
-            conn = sqlite3.connect('tanks.sqlite')
+            conn = sqlite3.connect(DB_PATH)
             c = conn.cursor()
             key = getKey(8)
             c.execute("INSERT INTO players (name, key, state, code) VALUES (?,?,?,?)", [name, key, "waiting", None])
@@ -70,7 +72,7 @@ class RegisterHandler(tornado.web.RequestHandler):
 
 class StatsHandler(tornado.web.RequestHandler):
     def get(self):
-        conn = sqlite3.connect('tanks.sqlite')
+        conn = sqlite3.connect(DB_PATH)
         gamestate=[]
         c = conn.cursor()
         c.execute("SELECT * FROM players")
@@ -110,14 +112,14 @@ class StatsHandler(tornado.web.RequestHandler):
         self.render("stats.html", gamestate = sorted(gamestate, key=lambda k: -k['score']))
 class GameHandler(tornado.web.RequestHandler):
     def get(self):
-        conn = sqlite3.connect('tanks.sqlite')
+        conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
         c.execute("SELECT * FROM settings")
         result = c.fetchall()
         settings = dict()
         for string in result:
             settings[string[1]] = string[2]
-        with open('map.txt') as map_file:
+        with open(MAP_PATH) as map_file:
             map_data = map_file.read()
             mainMap = map_data.split('\n')
             for i in range(len(mainMap)):
@@ -128,14 +130,14 @@ class GameHandler(tornado.web.RequestHandler):
 
 class StateHandler(tornado.web.RequestHandler):
     def get(self):
-        conn = sqlite3.connect('tanks.sqlite')
+        conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
         c.execute("SELECT * FROM settings")
         result = c.fetchall()
         settings = dict()
         for string in result:
             settings[string[1]] = string[2]
-        with open('map.txt') as map_file:
+        with open(MAP_PATH) as map_file:
             map_data = map_file.read()
             mainMap = map_data.split('\n')
             for i in range(len(mainMap)):
