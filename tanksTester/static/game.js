6ba9cca18e106acc2e6d441dd98b597e397ec211
roctbb <roctbb@gmail.com>
2018-11-18 12:38:35 +0300

    update tanks js

--- tanksTester/static/game.js
+++ tanksTester/static/game.js
@@ -3,21 +3,49 @@ var width = 1200;
 var height = 720;
 var textures = {};
 var tileSize = 40;
-var fetchIntervalId;
+var fetchIntervalId = undefined;
+var animationFrames = 15;
+var $$ = {
+    original_map: undefined,
+    map: undefined,
+    players: undefined,
+    tick: 5
+};
 
-main();
+function equalNames(name1, name2) {
+    if (typeof name1 !== "string") return false;
+    if (typeof name2 !== "string") return false;
+    if (name1 === name2) return true;
+    if (name1 === name2.split('(')[0]) return true;  // Some bicycles
+    return false;
+}
 
-window.addEventListener('load', function(){
-    canvas = document.getElementById('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    ctx = canvas.getContext('2d');
+function estimateMovement(map, x, y) {
+    var p = map[x][y];
+    var name = p.name;
+    if (name in $$.players) {
+        var suffix = 1;
+        while ((name + '(' + suffix.toString() + ')') in $$.players) suffix++;
+        name = name + '(' + suffix.toString() + ')';
+    }
+    var action = 'stay';
+    if ($$.original_map !== undefined) {
+        if (x > 0 && equalNames(p.name, $$.original_map[x - 1][y].name)) {
+            action = 'go_right';
+        } else if (x < map.length - 1 && equalNames(name, $$.original_map[x + 1][y].name)) {
+            action = 'go_left';
+        } else if (y > 0 && equalNames(p.name, $$.original_map[x][y - 1].name)) {
+            action = 'go_down';
+        } else if (y < map[x].length - 1 && equalNames(name, $$.original_map[x][y + 1].name)) {
+            action = 'go_up';
+        }
+    }
+    $$.players[name] = {
+        x: x, y: y, life: p['life'], hit: p['hit'], action: action
+    }
+}
 
-    fetchIntervalId = setTimeout(fetchState, 600);
-    // setInterval(redraw, 2000, field);
-});
-
-function redraw(map) {
+function preprocessing(map) {
     if (width !== window.innerWidth || height !== window.innerHeight) {
         width = window.innerWidth;
         height = window.innerHeight;
@@ -31,56 +59,111 @@ function redraw(map) {
             canvas.width = height * 30 / 18.0;
         }
         tileSize = canvas.width / 30.0;
+        ctx.font = (tileSize / 3.8).toString() + "px Arial";
     }
-    ctx.clearRect(0, 0, width, height);
+
+    $$.map = [];
+    $$.players = {};
     for (var x = 0; x < map.length; x++) {
-        for (var y = 0; y < map[0].length; y++) {
+        var map_row = [];
+        for (var y = 0; y < map[x].length; y++) {
+            var map_cell = [];
             switch (map[x][y]) {
                 case '#':
-                    ctx.drawImage(textures.brick, x * tileSize, y * tileSize, tileSize, tileSize);
+                    map_cell.push('#');
                     break;
                 case '@':
-                    ctx.drawImage(textures.coin, x * tileSize, y * tileSize, tileSize, tileSize);
+                    map_cell.push('@');
                     if ((x > 0 && (map[x - 1][y] === '&larr;' || map[x - 1][y] === '&rarr;')) ||
                         (x < map.length - 1 && (map[x + 1][y] === '&larr;' || map[x + 1][y] === '&rarr;'))) {
-                        ctx.drawImage(textures.horiz, x * tileSize, y * tileSize, tileSize, tileSize);
+                        map_cell.push('-');
                     }
                     if ((y > 0 && (map[x][y - 1] === '&uarr;' || map[x][y - 1] === '&darr;')) ||
                         (y < map[0].length - 1 && (map[x][y + 1] === '&uarr;' || map[x][y + 1] === '&darr;'))) {
-                        ctx.drawImage(textures.verti, x * tileSize, y * tileSize, tileSize, tileSize);
+                        map_cell.push('|');
                     }
                     break;
                 case '&larr;':
                 case '&rarr;':
-                    ctx.drawImage(textures.horiz, x * tileSize, y * tileSize, tileSize, tileSize);
+                    map_cell.push('-');
                     if ((y > 0 && (map[x][y - 1] === '&uarr;' || map[x][y - 1] === '&darr;')) ||
                         (y < map[0].length - 1 && (map[x][y + 1] === '&uarr;' || map[x][y + 1] === '&darr;'))) {
-                        ctx.drawImage(textures.verti, x * tileSize, y * tileSize, tileSize, tileSize);
+                        map_cell.push('|');
                     }
                     break;
                 case '&uarr;':
                 case '&darr;':
-                    ctx.drawImage(textures.verti, x * tileSize, y * tileSize, tileSize, tileSize);
+                    map_cell.push('|');
                     if ((x > 0 && (map[x - 1][y] === '&larr;' || map[x - 1][y] === '&rarr;')) ||
                         (x < map.length - 1 && (map[x + 1][y] === '&larr;' || map[x + 1][y] === '&rarr;'))) {
-                        ctx.drawImage(textures.horiz, x * tileSize, y * tileSize, tileSize, tileSize);
+                        map_cell.push('-');
                     }
                     break;
                 case '.':
                     break;
                 default:
-                    ctx.drawImage(textures.player, x * tileSize, y * tileSize, tileSize, tileSize);
-                    ctx.fillStyle = '#ffffff';
-                    var w_name = ctx.measureText(map[x][y].name).width;
-                    var w_life = ctx.measureText(map[x][y].life.toString()).width;
-                    ctx.fillText(map[x][y].name, (x + 0.5) * tileSize - w_name / 2, (y + 0.3) * tileSize);
-                    if (map[x][y].hit === 1) ctx.fillStyle = '#ff0000';
-                    ctx.fillText(map[x][y].life.toString(), (x + 0.5) * tileSize - w_life / 2,(y + 0.6) * tileSize);
-                    break;
+                    estimateMovement(map, x, y);
+            }
+            map_row.push(map_cell);
+        }
+        $$.map.push(map_row);
+    }
+    $$.original_map = map;
+}
+
+function animate() {
+    ctx.clearRect(0, 0, width, height);
+    for (var x = 0; x < $$.map.length; x++) {
+        for (var y = 0; y < $$.map[x].length; y++) {
+            for (var k = 0; k < $$.map[x][y].length; k++) {
+                switch($$.map[x][y][k]) {
+                    case '#':
+                        ctx.drawImage(textures['brick'], x * tileSize, y * tileSize, tileSize, tileSize);
+                        break;
+                    case '@':
+                        ctx.drawImage(textures['coin'], x * tileSize, y * tileSize, tileSize, tileSize);
+                        break;
+                    case '-':
+                        ctx.drawImage(textures['horiz'], x * tileSize, y * tileSize, tileSize, tileSize);
+                        break;
+                    case '|':
+                        ctx.drawImage(textures['verti'], x * tileSize, y * tileSize, tileSize, tileSize);
+                        break;
+                }
             }
         }
     }
-
+    for (var name in $$.players) {
+        if (!$$.players.hasOwnProperty(name)) continue;
+        var p = $$.players[name];
+        var baseX = p.x;
+        var baseY = p.y;
+        switch (p.action) {
+            case 'go_left':
+                baseX += $$.tick / animationFrames;
+                break;
+            case 'go_right':
+                baseX -= $$.tick / animationFrames;
+                break;
+            case 'go_up':
+                baseY += $$.tick / animationFrames;
+                break;
+            case 'go_down':
+                baseY -= $$.tick / animationFrames;
+                break;
+        }
+        ctx.drawImage(textures['player'], baseX * tileSize, baseY * tileSize, tileSize, tileSize);
+        ctx.fillStyle = '#ffffff';
+        var w_name = ctx.measureText(name).width;
+        var w_life = ctx.measureText(p.life.toString()).width;
+        ctx.fillText(name, (baseX + 0.5) * tileSize - w_name / 2, (baseY + 0.3) * tileSize);
+        if (p.hit === 1) ctx.fillStyle = '#ff0000';
+        ctx.fillText(p.life.toString(), (baseX + 0.5) * tileSize - w_life / 2, (baseY + 0.6) * tileSize);
+    }
+    $$.tick--;
+    if ($$.tick >= 0) {
+        setTimeout(animate, 800 / animationFrames);
+    }
 }
 
 function fetchState() {
@@ -91,17 +174,28 @@ function fetchState() {
         if (xhr.readyState !== 4) return;
         if (xhr.status === 200) {
             var map = JSON.parse(xhr.responseText);
-            redraw(map);
+            preprocessing(map);
+            $$.tick = animationFrames;
+            animate();
             fetchIntervalId = setTimeout(fetchState, 900);
         }
     }
 }
 
-function main() {
+function loadTextures() {
     var files = ["player", "grass", "brick", "coin", "horiz", "verti"];
     for (var i = 0; i < files.length; i++) {
         textures[files[i]] = new Image();
         textures[files[i]].src = '/static/res/' + files[i] + '.png';
-        // textures[files[i]].src = 'res/' + files[i] + '.png';
     }
 }
+
+loadTextures();
+
+window.addEventListener('load', function(){
+    canvas = document.getElementById('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    ctx = canvas.getContext('2d');
+    fetchIntervalId = setTimeout(fetchState, 600);
+});
